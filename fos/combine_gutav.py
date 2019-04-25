51ce4c7b23bc734156f42eeb95202e029bb99246
Will Broniec <williambroniec@gmail.com>
2019-04-24 20:02:46 -0400

    cleaning up some of my code

--- fos/combine_gutav.py
+++
@@ -1,3 +0,0 @@
-import sys, json
-
-f = open(sys.argv[1], 'r')
--- fos/replace_single_quote_with_double.py
+++
@@ -1,7 +0,0 @@
-import sys
-f = open(sys.argv[1], 'r', encoding='utf-8')
-r = f.readline()
-
-while r:
-	print(r[:-1].replace('\'', '"').encode('utf-8'))
-	r = f.readline()
